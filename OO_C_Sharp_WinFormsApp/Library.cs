411d300b1ec8c3478cd11120e774c93e8551880b
ICE <ice.chiruno9@gmail.com>
2023-06-09 15:51:07 +0900

    Library setLocation / setSize

--- OO_C_Sharp_WinFormsApp/Library.cs
+++ OO_C_Sharp_WinFormsApp/Library.cs
@@ -275,11 +275,12 @@ namespace OO_C_Sharp_WinFormsApp
 
             if (application.isDebugMode())
             {
-
+                setLocation(0,0).setSize(1000, 500);
                 // ここにデバッグモードの処理を記述できます
             }
             else
             {
+                setLocation(100, 0).setSize(500, 500);
 
                 // ここに通常モードの処理を記述できます
 
@@ -292,14 +293,12 @@ namespace OO_C_Sharp_WinFormsApp
         public Library setLocation(int x,int y)
         {
             this.Location = new Point(x,y);
-            update();
             return this;
         }
 
         public Library setSize(int width,int height)
         {
             this.Size = new Size(width,height);
-            update();
             return this;
         }
 
