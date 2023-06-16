dce7a835c8707c150c1f32d9566b9b0de983b921
miki@oic.jp <miki@oic.jp>
2023-06-16 15:37:31 +0900

    コンテキストメニューのサンプルを追加

---
+++ OO_C_Sharp_WinFormsApp/ToolStripMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_C_Sharp_WinFormsApp
+{
+
+    public class SampleToolStripMenuItem : ToolStripMenuItem
+    {
+
+        public SampleToolStripMenuItem()
+        {
+
+            Name = "SampleToolStripMenuItem";
+            Text = "サンプルメニュー";
+
+        }
+
+    }
+
+}
